68ffd14740065a4885781e97655b88c72fd203b1
avi leventhal <avi@leventhal.co.il>
2025-12-06 16:33:12 +0200

    Cleanup: Removed deprecated utils.js file after consolidating all helpers into utils/index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,22 @@
 
 
 //index.ts
-export function createPageUrl(pageName: string) {
-    return '/' + pageName.toLowerCase().replace(/ /g, '-');
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs))
 }
+
+export function createPageUrl(pageName: string): string {
+  if (!pageName) return "/";
+  return "/" + pageName.toLowerCase().replace(/\s/g, "-");
+};
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+  }).format(amount);
+};
