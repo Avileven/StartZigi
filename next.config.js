030a323ed16aef41a58b54ef69566998bf149096
avi leventhal <avi@leventhal.co.il>
2025-12-08 10:51:15 +0200

    fix: add URL redirects

--- next.config.js
+++ next.config.js
@@ -7,6 +7,25 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/Home',
+        destination: '/home',
+        permanent: true,
+      },
+      {
+        source: '/Dashboard',
+        destination: '/dashboard',
+        permanent: true,
+      },
+      {
+        source: '/CreateVenture',
+        destination: '/create-venture',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
