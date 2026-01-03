140ff11b810d3dd13d7a8e4812f28570e191efc3
avi leventhal <avi@leventhal.co.il>
2026-01-03 10:15:16 +0200

    change LayoutWrapper

--- src/components/layout/LayoutWrapper.jsx
+++ src/components/layout/LayoutWrapper.jsx
@@ -1,7 +1,18 @@
+
+// /C:\STRARTZIG\src\components\layout\LayoutWrapper
 "use client";
 
 import ClientLayout from "./ClientLayout";
+import { usePathname } from "next/navigation";
 
 export default function LayoutWrapper({ children }) {
+  const pathname = usePathname();
+
+  // ✅ [2026-01-03] HOME (/) בלי דשבורד
+  if (pathname === "/") {
+    return <>{children}</>;
+  }
+
   return <ClientLayout>{children}</ClientLayout>;
 }
+
