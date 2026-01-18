d2818db1f0335789471238ba8ed0df238642473a
avi leventhal <avi@leventhal.co.il>
2026-01-18 17:56:21 +0200

    UPDATE NEXTCONFI

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +9,28 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      // startzig.vercel.app -> www.startzig.com
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "startzig.vercel.app" }],
+        destination: "https://www.startzig.com/:path*",
+        permanent: true,
+      },
+
+      // startzig.com -> www.startzig.com (אם יש לך גם בלי www)
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "startzig.com" }],
+        destination: "https://www.startzig.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
+
+
