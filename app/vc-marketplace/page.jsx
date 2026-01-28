6d9e4b94f97fbe4ffcbb0a11109578016b5ac8c6
avi leventhal <avi@leventhal.co.il>
2026-01-28 09:34:32 +0200

    UPDATE vcmarketplace 2

--- app/vc-marketplace/page.jsx
+++ app/vc-marketplace/page.jsx
@@ -6,7 +6,7 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Link } from 'react-router-dom';
+import Link from "next/link";
 import { createPageUrl } from '@/utils';
 import { 
   Building2, 
