849efbdf1e33f34b16c86d3066e37d8b3f9c461e
avi leventhal <avi@leventhal.co.il>
2026-02-07 19:13:28 +0200

    change GLOBALS

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 border-b border-white/10 bg-gradient-to-b from-indigo-500 to-black to-30%">
+    <nav className="fixed top-0 left-0 right-0 z-50 border-b border-white/10 bg-gradient-to-b from-indigo-500 to-black to-75%">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           
