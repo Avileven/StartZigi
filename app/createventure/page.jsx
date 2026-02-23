8226fb2c162ec3cf23e8d744a2e291c624410136
avi leventhal <avi@leventhal.co.il>
2026-02-23 09:14:48 +0200

    UPDATE create venture5

--- app/createventure/page.jsx
+++ app/createventure/page.jsx
@@ -347,7 +347,7 @@ function CreateVentureForm() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 p-4 md:p-8 pt-20">
-      <div className="max-w-6xl mx-auto">
+      <div className="max-w-5xl mx-auto">
         <div className="flex flex-col md:flex-row gap-6 items-start">
 
           {/* עמודה ראשית - Create Your Venture */}
