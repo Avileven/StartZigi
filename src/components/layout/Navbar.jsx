eb053a3c91ae8193856f5c181d6a9e6a8c36e07c
Avileven <avi@leventhal.il>
2026-09-15 07:41:06 +0300

    ADD NEW ICON TO NAVBAR2

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
           <div className="flex-shrink-0">
             <Link href="/" className="flex items-center gap-2">
               <img src="/startzig-navbar-icon.png" alt="" className="h-12 md:h-11 object-contain" />
-              <img src="/startzig-wordmark.png" alt="StartZig" className="h-[70px] md:h-16 object-contain cursor-pointer" />
+              <span className="text-2xl font-bold bg-gradient-to-r from-fuchsia-400 to-violet-500 bg-clip-text text-transparent cursor-pointer">StartZig</span>
             </Link>
           </div>
 
